35674f58dd3ebb569a16b58805a39d7814576b06
Nicolas <NicolasStoupy@gmail.com>
2025-11-02 12:55:06 +0100

    Add Infrastructure layer

---
+++ src/Infrastructure/DependencyInjection.cs
@@ -0,0 +1,37 @@
+﻿using Application.Common.Interfaces;
+using Infrastructure.Data;
+using Infrastructure.Data.Interceptors;
+using Infrastructure.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static void AddInfrastructureServices(this IHostApplicationBuilder builder)
+        {
+            var connectionString = builder.Configuration.GetConnectionString("DB");
+            builder.Services.AddScoped<ISaveChangesInterceptor, AuditableEntityInterceptor>();
+            builder.Services.AddScoped<ISaveChangesInterceptor, DispatchDomainEventsInterceptor>();
+            builder.Services.AddDbContext<ApplicationDbContext>((sp, options) =>
+            {
+                options.AddInterceptors(sp.GetServices<ISaveChangesInterceptor>());
+                options.UseSqlServer(connectionString);
+                options.ConfigureWarnings(warnings => warnings.Ignore(RelationalEventId.PendingModelChangesWarning));
+            });
+            builder.Services
+                .AddDefaultIdentity<ApplicationUser>()
+                .AddRoles<IdentityRole>()
+                 .AddEntityFrameworkStores<ApplicationDbContext>();
+            builder.Services.AddScoped<IApplicationDbContext>(sp =>
+              sp.GetRequiredService<ApplicationDbContext>());
+            builder.Services.AddSingleton(TimeProvider.System);
+            builder.Services.AddScoped<IApplicationDbContext>(provider => provider.GetRequiredService<ApplicationDbContext>());
+        }
+    }
+}
