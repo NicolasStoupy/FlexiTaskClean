7afc687ff8b0b400ae1f2867aaf1a58d5f528632
Nicolas <NicolasStoupy@gmail.com>
2025-11-02 12:56:12 +0100

    Add Web Layer

---
+++ src/Web/DependencyInjection.cs
@@ -0,0 +1,30 @@
+﻿using Application.Common.Interfaces;
+using Azure.Identity;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using Web.Services;
+
+namespace Web
+{
+    public static class DependencyInjection
+    {
+        public static void AddWebServices(this IHostApplicationBuilder builder)
+        {
+          
+
+            builder.Services.AddScoped<IUser, FakeUser>();
+
+            builder.Services.AddHttpContextAccessor();
+
+
+            // Customise default API behaviour
+            builder.Services.Configure<ApiBehaviorOptions>(options =>
+                options.SuppressModelStateInvalidFilter = true);
+
+            builder.Services.AddEndpointsApiExplorer();
+
+           
+        }
+
+    }
+}
---
+++ src/Web/Services/FakeUser.cs
@@ -0,0 +1,11 @@
+﻿using Application.Common.Interfaces;
+
+namespace Web.Services
+{
+    public class FakeUser : IUser
+    {
+        public string? Id => "BE081801";
+
+        public List<string>? Roles => new List<string>();
+    }
+}
